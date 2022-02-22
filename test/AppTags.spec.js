4ce1148798113f5461deec5fcce99b02a0ac9ac3
Leigh <leighdinaya@protonmail.com>
2022-02-22 14:16:03 +0800

    feat: add unit tests

--- test/AppTags.spec.js
+++ test/AppTags.spec.js
@@ -2,12 +2,14 @@ import { mount } from '@vue/test-utils'
 import AppTags from '@/components/AppTags.vue'
 
 describe('AppTags', () => {
-  test('is a Vue instance', () => {
+  test('renders correct tags', () => {
     const wrapper = mount(AppTags, {
       propsData: {
         tags: ['nuxtjs', 'tailwindcss', 'vuejs'],
       },
     })
     expect(wrapper.vm.tags).toEqual(['nuxtjs', 'tailwindcss', 'vuejs'])
+    expect(wrapper.findAll('button').length).toBe(3)
+    expect(wrapper.findAll('button').at(0).text()).toBe('nuxtjs')
   })
 })
