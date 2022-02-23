691af9f8c7dd81b8070b7207b6f99bbabba49863
Leigh <leighdinaya@protonmail.com>
2022-02-23 10:43:36 +0800

    feat: update

--- components/AppNewsLetter.vue
+++ components/AppNewsLetter.vue
@@ -9,37 +9,39 @@
         </p>
       </div>
       <div class="flex-1 mt-5">
-        <form
-          id="newsletter"
-          class="email-form flex"
-          name="newsletter"
-          method="POST"
-          data-netlify="true"
-          netlify-honeypot="bot-field"
-        >
-          <div hidden aria-hidden="true">
-            <label>
-              Don’t fill this out if you're human:
-              <input name="bot-field" />
-            </label>
-          </div>
-          <div class="flex">
-            <input
-              id="email"
-              type="email"
-              name="email"
-              placeholder="Email"
-              required
-              class="w-2/3 flex-1 p-2 rounded-lg"
-            />
-            <button
-              type="submit"
-              class="bg-slate-800 text-slate-100 ml-4 p-2 rounded-lg"
-            >
-              Subscribe
-            </button>
-          </div>
-        </form>
+        <client-only>
+          <form
+            id="newsletter"
+            class="email-form flex"
+            name="newsletter"
+            method="POST"
+            data-netlify="true"
+            netlify-honeypot="bot-field"
+          >
+            <div hidden aria-hidden="true">
+              <label>
+                Don’t fill this out if you're human:
+                <input name="bot-field" />
+              </label>
+            </div>
+            <div class="flex">
+              <input
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Email"
+                required
+                class="w-2/3 flex-1 p-2 rounded-lg"
+              />
+              <button
+                type="submit"
+                class="bg-slate-800 text-slate-100 ml-4 p-2 rounded-lg"
+              >
+                Subscribe
+              </button>
+            </div>
+          </form>
+        </client-only>
       </div>
     </div>
   </div>
