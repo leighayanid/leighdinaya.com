cf3e6fee8bfab1fee2fb09a32ee2ac3fd1b28379
Leigh <leighdinaya@protonmail.com>
2022-02-23 19:47:44 +0800

    feat: update

--- components/AppNewsLetter.vue
+++ components/AppNewsLetter.vue
@@ -60,7 +60,7 @@ export default {
         const res = await fetch('/.netlify/functions/buttondown', {
           method: 'POST',
           body: {
-            email: form.email,
+            form,
           },
           headers: {
             'Content-Type': 'application/json',
