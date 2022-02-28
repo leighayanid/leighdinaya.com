296da1e506eec7214d69d8ff037c791eda47de39
Leigh <leighdinaya@protonmail.com>
2022-02-28 08:11:43 +0800

    feat: updates

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="z-50">
     <nuxt-link to="/" class="font-bold text-lg">LD</nuxt-link>
   </div>
 </template>
--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -3,10 +3,7 @@
     v-show="$store.state.toggle.toggle"
     class="w-full h-screen dark:bg-zinc-800 bg-slate-100 z-auto rounded-b-lg py-10 px-8 shadow-md transition-opacity"
   >
-    <base-transition>
-      <lazy-app-logo v-if="$store.state.toggle.toggle" />
-    </base-transition>
-    <hr class="mt-7 my-5" />
+    <hr class="mt-10 my-5" />
     <div class="text-xl" @click="close">
       <base-transition-group class="flex flex-col">
         <nuxt-link v-if="$store.state.toggle.toggle" key="1" to="/blog"
