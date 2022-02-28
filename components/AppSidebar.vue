07a65642e4da771ca6620ce540835219ce66f2f9
Leigh <leighdinaya@protonmail.com>
2022-02-28 08:00:17 +0800

    feat: updates

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -4,9 +4,9 @@
     class="w-full h-screen dark:bg-zinc-800 bg-slate-100 z-auto rounded-b-lg py-10 px-8 shadow-md transition-opacity"
   >
     <base-transition>
-      <lazy-app-logo v-if="$store.state.toggle.toggle" class="px-4" />
+      <lazy-app-logo v-if="$store.state.toggle.toggle" />
     </base-transition>
-    <hr class="mt-10 my-5" />
+    <hr class="mt-7 my-5" />
     <div class="text-xl" @click="close">
       <base-transition-group class="flex flex-col">
         <nuxt-link v-if="$store.state.toggle.toggle" key="1" to="/blog"
@@ -38,6 +38,6 @@ export default {
 
 <style scoped>
 a {
-  @apply m-2 py-2;
+  @apply py-2 text-base;
 }
 </style>
