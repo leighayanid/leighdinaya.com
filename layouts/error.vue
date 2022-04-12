b275cded67f90a3f2defdd12ed0cdeb1bb369de0
Leigh <leighdinaya@protonmail.com>
2022-04-13 07:42:01 +0800

    feat: add error page

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div v-if="error.statusCode === 404">
+      <h1 class="text-8xl font-extrabold">404</h1>
+      <p class="text-xl font-semibold">Page not found.</p>
+      <NuxtLink to="/" class="text-purple-500">Home page</NuxtLink>
+    </div>
+    <h1 v-else class="text-xl font-semibold">An error occurred</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'none',
+  props: ['error'],
+}
+</script>
---
+++ layouts/none.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container grid items-center">
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
