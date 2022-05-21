c6f0f73f988a8c38c4c9e6d8e0e8dbc7805833fb
Leigh <leighdinaya@protonmail.com>
2022-05-21 08:04:04 +0800

    feat: animate background shapes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,25 @@ module.exports = {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
       },
+      animation: {
+        blob: 'blob 7s infinite',
+      },
+      keyframes: {
+        blob: {
+          '0%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+          '33%': {
+            transform: 'translate(30px, -50px) scale(1.1)',
+          },
+          '68%': {
+            transform: 'translate(-20px, 20px) scale(0.9)',
+          },
+          '100%': {
+            transform: 'translate(0px, 0px) scale(1)',
+          },
+        },
+      },
     },
   },
 }
